bbeced88b9a0ec4bfb18ae86816cee32c316fb53
Animesh Sharma <animesharma08+github@gmail.com>
2021-05-22 19:53:18 +0530

    Added option to send email notification

--- src/index.py
+++ src/index.py
@@ -2,7 +2,9 @@ import requests
 import argparse
 import re
 import sys
-from datetime import datetime
+import json
+import smtplib
+from email.mime.text import MIMEText
 from random import randint
 from time import sleep, strftime
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -28,21 +30,24 @@ class CowinVaccineTracker:
                             dest="vaccine",
                             choices=["COVAXIN", "COVISHIELD"],
                             help="Preferred Vaccine: COVAXIN or COVISHIELD. Shows any available by default")
+        parser.add_argument("--recipient", 
+                            dest="recipient",
+                            help="Email address to notify vaccine availability")
         parser.add_argument("--loop",
                             dest="loop",
                             action="store_true",
                             help="Specify option to run script on a loop")
-    
+    @staticmethod
     def terminate(error):
         print("Error: " + error)
         sys.exit(-1)
 
     def validate_input(self, args):
-        if not args.pin:
-            self.terminate("Valid PIN Code not found")
         for pin in args.pin:
-            if not re.match(r"^\d\d\d\d\d\d$", pin):
+            if not re.fullmatch(r"^\d\d\d\d\d\d$", pin):
                 self.terminate("Invalid PIN Code")
+        if args.recipient and not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", args.recipient):
+            self.terminate("Invalid recipient email address")
 
     def get_vaccine_centers_by_pin(self, pin, date):
         api_endpoint = "/v2/appointment/sessions/public/calendarByPin"
@@ -50,7 +55,7 @@ class CowinVaccineTracker:
             "pincode": pin,
             "date": date
         }
-        response = requests.get(url=self.base_url + api_endpoint, params=parameters, headers={"User-Agent": "PostmanRuntime/8.3.1"})
+        response = requests.get(url=self.base_url + api_endpoint, params=parameters, headers={"User-Agent": "PostmanRuntime/8"})
         if response.ok:
             return response.json().get("centers")
         else:
@@ -70,18 +75,34 @@ class CowinVaccineTracker:
         return filtered_centers
 
     @staticmethod
-    def display_centers(centers):
-        if not centers:
-            print("No available slots, please try again later.")
-            return
-        for center in centers:
-            print(center.get("pincode"))
-            for session in center.get("sessions"):
-                print(center.get("name"), " - ", center.get("address"), " - ", center.get("district_name"), " - ", center.get("state_name"))
-                print("Date: ", session.get("date"))
-                print("Available Capacity: ", session.get("available_capacity"))
-                print("Minimum Age Limit: ", session.get("min_age_limit"))
-                print("Vaccine: ", session.get("vaccine"), "\n") 
+    def send_email_notification(recipient, content):
+        email_config = json.loads("./config/email_config.json")
+        sender = email_config.get("sender_email")
+        message = MIMEText(content)
+        message["Subject"] = "Vaccine Availability Notification"
+        message["From"] = sender
+        message["To"] = recipient
+        with smtplib.SMTP_SSL(email_config.get("smtp_url"), email_config.get("smtp_port")) as mail_server:
+            mail_server.login(sender, email_config.get("password"))
+            mail_server.sendmail(sender, recipient, message.as_string())
+
+    @staticmethod
+    def create_formatted_message(centers):
+        message_body = "Available Vaccine Centers:\n\n"
+        for index, center in enumerate(centers):
+            message_body += "Center " + str(index + 1) + ": " + center.get("name") + " - " + \
+                            center.get("address") + " - " + center.get("district_name") + " - " + \
+                            center.get("state_name") + " - " + str(center.get("pincode")) + "\n\n"
+
+            for index, session in enumerate(center.get("sessions")):   
+                message_body += "Session " + str(index + 1) + ":\n\n"         
+                message_body += "\tDate: " + session.get("date") + "\n"
+                message_body += "\tAvailable Capacity: " + str(session.get("available_capacity")) + "\n"
+                message_body += "\tMinimum Age Limit: " + str(session.get("min_age_limit")) + "\n"
+                message_body += "\tVaccine: " + session.get("vaccine") + "\n"
+                message_body += "\tFee Type: " + center.get("fee_type") + "\n\n"
+        message_body += "Book your slot now at: https://selfregistration.cowin.gov.in/\n\n"
+        return message_body
 
 if __name__ == "__main__":
     tracker = CowinVaccineTracker()
@@ -93,12 +114,16 @@ if __name__ == "__main__":
             for future in as_completed(future_centers):
                 pin = future_centers[future]
                 try:
-                    vaccine_centers = future.result()
+                    vaccine_centers = tracker.filter_centers_by_attribute(future.result(), None, None)
                     if(args.min_age):
                         vaccine_centers = tracker.filter_centers_by_attribute(vaccine_centers, "min_age_limit", args.min_age)
                     if(args.vaccine):
                         vaccine_centers = tracker.filter_centers_by_attribute(vaccine_centers, "vaccine", args.vaccine.upper())
-                    tracker.display_centers(vaccine_centers)
+                    if vaccine_centers: 
+                        message_body = tracker.create_formatted_message(vaccine_centers)
+                        print(message_body)
+                        if args.recipient:
+                            tracker.send_email_notification(args.recipient, message_body)
                 except Exception as exc:
                     print('%r generated an exception: %s' % (pin, exc))
         if not args.loop:
