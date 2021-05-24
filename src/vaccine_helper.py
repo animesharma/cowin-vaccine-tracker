1c4d25a8c25c3f47607c14fc3a0c7623e7d46616
Animesh Sharma <animesharma08+github@gmail.com>
2021-05-24 10:46:37 +0530

    Minor bugfix in email rate-limiting

--- src/vaccine_helper.py
+++ src/vaccine_helper.py
@@ -11,7 +11,7 @@ class VaccineHelper:
     def __init__(self):
         """Constructor"""
         self.base_url = "https://cdn-api.co-vin.in/api"
-        self.sent_email_checksums = {}
+        self.sent_email_checksums = set()
 
     @staticmethod
     def terminate(error):
